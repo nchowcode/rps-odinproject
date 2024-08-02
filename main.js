83962b43f10b909243223caa096d1911081de342
Nathan Chow <134994761+nchowcode@users.noreply.github.com>
2024-08-01 19:04:28 -0700

    Add function to get user choice
    Prompt user to select a number that correlates to a choice, returns the value from pair

--- main.js
+++ main.js
@@ -5,4 +5,20 @@ function getComputerChoice(){
     
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(){
+    let humanChoice; 
+    const choices = {1: 'rock', 2: 'paper', 3: 'scissors'};
+    while (true) {
+        humanChoice = window.prompt("Choose an option:\n1: Rock\n2: Paper\n3: Scissors\n");
+        humanChoice = parseInt(humanChoice);
+
+        if (choices[humanChoice]){
+            return choices[humanChoice];    //  rock/paper/scissor
+        }
+        else{
+            alert("Invalid choice. Please choose a number between 1 and 3.");
+        }
+    }
+} 
+
+console.log(getHumanChoice());
